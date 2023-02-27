ec2f30376fba2b0d7cf8c84a8410e94d3578f91e
MarcosAGRocha <125692063+MarcosRochaDevSc@users.noreply.github.com>
2023-02-27 13:38:17 -0300

    Update app.js

--- [M1S06]/M1S06 Ex2/app.js
+++ [M1S06]/M1S06 Ex2/app.js
@@ -1,8 +1,8 @@
 function mostrarHora() {
   const data = new Date();
-  const horas = data.getHours().toString().padStart(2, "0");
-  const minutos = data.getMinutes().toString().padStart(2, "0");
-  const segundos = data.getSeconds().toString().padStart(2, "0");
+  const horas = data.getHoras().toString().padStart(2, "0");
+  const minutos = data.getMinutos().toString().padStart(2, "0");
+  const segundos = data.getSegundos().toString().padStart(2, "0");
   console.log(`${horas}:${minutos}:${segundos}`);
 }
 
