434bc9dd6cd1d5488316cec7f9feac48e1209403
MarcosRochaDevSc <125692063+MarcosRochaDevSc@users.noreply.github.com>
2023-02-27 13:43:21 -0300

    Delete [M1S06]/M1S06 Ex2 directory

--- [M1S06]/M1S06 Ex2/app.js
+++
@@ -1,9 +0,0 @@
-function mostrarHora() {
-  const data = new Date();
-  const horas = data.getHoras().toString().padStart(2, "0");
-  const minutos = data.getMinutos().toString().padStart(2, "0");
-  const segundos = data.getSegundos().toString().padStart(2, "0");
-  console.log(`${horas}:${minutos}:${segundos}`);
-}
-
-setInterval(mostrarHora, 1000);
